45db6db5df4cd8922e6694c78cedfbe869b97a6e
CristopherIsGood <39185456+CristopherIsGood@users.noreply.github.com>
2018-06-04 18:37:50 +1000

    Update javascript-v1.js
    Version 1.01
    - Fade Removed

--- js/javascript-v1.js
+++ js/javascript-v1.js
@@ -39,19 +39,3 @@
 		function myFunction() {
 			document.getElementById("nav-drop_down").classList.toggle("show");
 		}
-		
-		$(window).on("load",function() {
-		  $(window).scroll(function() {
-			var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-			$(".form").each(function() {
-
-			  var objectBottom = $(this).offset().top + $(this).outerHeight();
-			  
-			  if (objectBottom < windowBottom) { 
-				if ($(this).css("opacity")==0) {$(this).fadeTo(300,1);}
-			  } else { 
-				if ($(this).css("opacity")==1) {$(this).fadeTo(300,0);}
-			  }
-			});
-		  }).scroll(); 
-		});
