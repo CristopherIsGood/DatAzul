061bd949e653fc4be64cfa57373d93aabc7dba79
Cris M <39185456+CristopherIsGood@users.noreply.github.com>
2018-11-02 13:10:16 +1100

    Add files via upload

--- js/javascript-v1.js
+++ js/javascript-v1.js
@@ -1,3 +1,5 @@
+// JavaScript - Version 1.01
+		
 		$(document).ready(function(){
 		
 		  $("a").on('click', function(event) {
@@ -16,26 +18,14 @@
 			  });
 			} 
 		  });
-		  
-			$(window).scroll( function(){
-			
-				$('.fade').each( function(i){
-					
-					var bottom_of_object = $(this).position().top + $(this).outerHeight();
-					var bottom_of_window = $(window).scrollTop() + $(window).height();
-					
-					if( bottom_of_window > bottom_of_object ){
-						
-						$(this).animate({'opacity':'1'},1500);
-							
-					}
-					
-				}); 
-			
-			});
+
+
 		  
 		});
 		
 		function myFunction() {
 			document.getElementById("nav-drop_down").classList.toggle("show");
+			$('ul li a').click(function(event){
+				$('.nav-list_mobile').removeClass('show');
+			});
 		}
