e36dc442abc5a6e728797e773769645c7a8d7419
Cris M <39185456+CristopherIsGood@users.noreply.github.com>
2018-11-05 14:21:16 +1100

    Add files via upload

--- js/javascript-v1.js
+++ js/javascript-v1.js
@@ -1,31 +1,33 @@
-// JavaScript - Version 1.01
-		
+// JavaScript - Version 1.04
+/* global $ */
+/* global document */
+/* global window */
+/* eslint no-mixed-spaces-and-tabs: ["error", "smart-tabs"] */
+/* exported myFunction */
+
 		$(document).ready(function(){
 		
-		  $("a").on('click', function(event) {
-
+		  $("a").click(function(event){
+			  
 			if (this.hash !== "") {
-
 			  event.preventDefault();
-
 			  var hash = this.hash;
-
-			  $('html, body').animate({
-				scrollTop: $(hash).offset().top
-			  }, 800, function(){
-		   
+			  $('html, body').animate({  
+				scrollTop: $(hash).offset().top 
+			  }, 1800, function(){
 				window.location.hash = hash;
 			  });
-			} 
+			}
+			  
+			$('.nav-list_mobile').removeClass('show');
+ 
 		  });
-
-
 		  
 		});
 		
+
 		function myFunction() {
+			
 			document.getElementById("nav-drop_down").classList.toggle("show");
-			$('ul li a').click(function(event){
-				$('.nav-list_mobile').removeClass('show');
-			});
+
 		}
