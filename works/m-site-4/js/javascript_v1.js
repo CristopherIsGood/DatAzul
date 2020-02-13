6028c0df171f8777ba5426a3590600334e2c663f
Cris M <39185456+CristobalMontesino@users.noreply.github.com>
2020-02-13 11:47:39 +1100

    Add files via upload

--- works/m-site-4/js/javascript_v1.js
+++ works/m-site-4/js/javascript_v1.js
@@ -1,1 +1,15 @@
+// JavaScript v1.0
+/* global $ */
+/* global document */
+/* global window */
+/* eslint no-mixed-spaces-and-tabs: ["error", "smart-tabs"] */
+/* exported myButton */
 
+$(document).ready(function(){	
+	// remove class when it closes the drop down list after "getElementById("nav-drop_down")"
+	$('.navigation').removeClass('show');
+});
+
+function myButton() {
+	document.getElementById("nav-drop_down").classList.toggle("show");
+}
