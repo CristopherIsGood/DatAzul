a370a6cfe52dc27928f8d3021ba83b999bccb3cd
Cris M <39185456+CristobalMontesino@users.noreply.github.com>
2020-02-18 15:03:05 +1100

    Add files via upload

---
+++ js/javascript_v1.js
@@ -0,0 +1,34 @@
+// JavaScript v1.0
+/* global $ */
+/* global document */
+/* global window */
+/* eslint no-mixed-spaces-and-tabs: ["error", "smart-tabs"] */
+/* exported myButton */
+
+$(window).scroll(function(){
+	var section1 = $("#section1")  
+	var section2 = $("#section2")
+	var section1_height = section1.height();  
+	var section2_height = section2.height();
+	
+    if ($(this).scrollTop() > section1_height) {
+       $('#section1').addClass('hidden');
+    } else {
+       $('#section1').removeClass('hidden');
+    }
+	
+    if ($(this).scrollTop() > section2_height) {
+       $('#section2').addClass('visible');
+    } else {
+       $('#section2').removeClass('visible');
+    }
+});
+
+$(document).ready(function(){	
+	// remove class when it closes the drop down list after "getElementById("nav-drop_down")"
+	$('.navigation').removeClass('show');
+});
+
+function myButton() {
+	document.getElementById("nav-drop_down").classList.toggle("show");
+}
