e0e367cb3d5346a2d9de68ab00e8d2091ead8801
Cris M <39185456+CristobalMontesino@users.noreply.github.com>
2020-02-18 11:23:40 +1100

    Add files via upload

--- works/m-site-4/js/javascript_v1.js
+++ works/m-site-4/js/javascript_v1.js
@@ -5,6 +5,20 @@
 /* eslint no-mixed-spaces-and-tabs: ["error", "smart-tabs"] */
 /* exported myButton */
 
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 360) {
+       $('#section1').addClass('hidden');
+    } else {
+       $('#section1').removeClass('hidden');
+    }
+	
+    if ($(this).scrollTop() > 360) {
+       $('#section2').addClass('visible');
+    } else {
+       $('#section2').removeClass('visible');
+    }
+});
+
 $(document).ready(function(){	
 	// remove class when it closes the drop down list after "getElementById("nav-drop_down")"
 	$('.navigation').removeClass('show');
