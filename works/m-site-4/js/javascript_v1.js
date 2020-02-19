934418b1ef0f207ba9280a8375c59cf11e63d3ea
Cris M <39185456+CristobalMontesino@users.noreply.github.com>
2020-02-19 11:25:20 +1100

    Add files via upload

--- works/m-site-4/js/javascript_v1.js
+++ works/m-site-4/js/javascript_v1.js
@@ -6,17 +6,37 @@
 /* exported myButton */
 
 $(window).scroll(function(){
-    if ($(this).scrollTop() > 360) {
+	var section1 = $("#section1")  
+	var section2 = $("#section2")
+	var section1_height = section1.height();  
+	var section2_height = section2.height();
+	
+    if ($(this).scrollTop() > section1_height) {
        $('#section1').addClass('hidden');
     } else {
        $('#section1').removeClass('hidden');
     }
 	
-    if ($(this).scrollTop() > 360) {
+    if ($(this).scrollTop() > section2_height) {
        $('#section2').addClass('visible');
     } else {
        $('#section2').removeClass('visible');
     }
+	
+	if ($(window).width() < 960) {
+	   //alert('Less than 960');
+		if ($(this).scrollTop() > section2_height) {
+		   $('#section1').addClass('hidden');
+		} else {
+		   $('#section1').removeClass('hidden');
+		}
+		
+		if ($(this).scrollTop() > section1_height) {
+		   $('#section2').addClass('visible');
+		} else {
+		   $('#section2').removeClass('visible');
+		}
+	}
 });
 
 $(document).ready(function(){	
