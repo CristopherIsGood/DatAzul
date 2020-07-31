a783dbe0578565d48e3b8d46cb9c5ae83d911794
Cris M <39185456+CristobalMontesino@users.noreply.github.com>
2020-07-31 13:16:08 +1000

    Add files via upload

--- works/m-site-5/js/javascript.js
+++ works/m-site-5/js/javascript.js
@@ -1,1 +1,20 @@
+// JavaScript Document
+/* global $ */
+/* global document */
+/* global window */
+/* eslint no-mixed-spaces-and-tabs: ["error", "smart-tabs", "no-unused-vars"] */
+/* exported openmenu */
+/* exported closemenu */
+function openmenu()
+{
+	document.getElementById("side-menu").style.display="block";
+	document.getElementById("menu-btn").style.display="none";
+	document.getElementById("close-btn").style.display="block";
+}
 
+function closemenu()
+{
+	document.getElementById("side-menu").style.display="none";
+	document.getElementById("menu-btn").style.display="block";
+	document.getElementById("close-btn").style.display="none";
+}
