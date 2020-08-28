d5e56caeb1dfa8d09d3a72215a0d3f3be09ffcaa
Cris M <39185456+CristobalMontesino@users.noreply.github.com>
2020-08-28 11:37:17 +1000

    Add files via upload

--- works/m-site-5/js/javascript.js
+++ works/m-site-5/js/javascript.js
@@ -1,20 +1,43 @@
-// JavaScript Document
+// JavaScript
 /* global $ */
 /* global document */
 /* global window */
 /* eslint no-mixed-spaces-and-tabs: ["error", "smart-tabs", "no-unused-vars"] */
-/* exported openmenu */
-/* exported closemenu */
-function openmenu()
-{
-	document.getElementById("side-menu").style.display="block";
-	document.getElementById("menu-btn").style.display="none";
-	document.getElementById("close-btn").style.display="block";
-}
 
-function closemenu()
-{
-	document.getElementById("side-menu").style.display="none";
-	document.getElementById("menu-btn").style.display="block";
-	document.getElementById("close-btn").style.display="none";
-}
+$(document).ready(function(){
+		$('.like-btn').on('click', function() {
+		   $(this).toggleClass('is-active');
+		});
+
+		$('.minus-btn').on('click', function(e) {
+			e.preventDefault();
+			var $this = $(this);
+			var $input = $this.closest('div').find('input');
+			var value = parseInt($input.val());
+
+			if (value > 1) {
+				value = value - 1;
+			} else {
+				value = 0;
+			}
+
+		  $input.val(value);
+
+		});
+
+		$('.plus-btn').on('click', function(e) {
+			e.preventDefault();
+			var $this = $(this);
+			var $input = $this.closest('div').find('input');
+			var value = parseInt($input.val());
+
+			if (value > 100) {
+				value = value + 1;
+			} else {
+				value = 100;
+			}
+
+			$input.val(value);
+		});
+});
+
