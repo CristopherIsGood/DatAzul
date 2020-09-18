ab2c60addbd78a74a68970b57bd0e460f90508ce
Cris M <39185456+CristobalMontesino@users.noreply.github.com>
2020-09-18 15:38:03 +1000

    Add files via upload

--- works/m-site-4/js/javascript_v1.js
+++ works/m-site-4/js/javascript_v1.js
@@ -2,41 +2,35 @@
 /* global $ */
 /* global document */
 /* global window */
-/* eslint no-mixed-spaces-and-tabs: ["error", "smart-tabs"] */
+/* eslint no-mixed-spaces-and-tabs: ["error", "smart-tabs", "no-unused-vars"] */
 /* exported myButton */
 
 $(window).scroll(function(){
 	var section1 = $("#section1")  
 	var section2 = $("#section2")
+	var section3 = $("#section3")
 	var section1_height = section1.height();  
 	var section2_height = section2.height();
+	var section3_height = section3.height();
 	
-    if ($(this).scrollTop() > section1_height) {
-       $('#section1').addClass('hidden');
+    if ($(this).scrollTop() > section1_height - 225) {
+        $('#section2').addClass('visible');
     } else {
-       $('#section1').removeClass('hidden');
-    }
-	
-    if ($(this).scrollTop() > section2_height) {
-       $('#section2').addClass('visible');
-    } else {
-       $('#section2').removeClass('visible');
-    }
-	
-	if ($(window).width() < 960) {
-	   //alert('Less than 960');
-		if ($(this).scrollTop() > section2_height) {
-		   $('#section1').addClass('hidden');
-		} else {
-		   $('#section1').removeClass('hidden');
-		}
-		
-		if ($(this).scrollTop() > section1_height) {
-		   $('#section2').addClass('visible');
-		} else {
-		   $('#section2').removeClass('visible');
-		}
+        $('#section2').removeClass('visible');
 	}
+	
+	if ($(this).scrollTop() > (section1_height + section2_height - 225) ) {
+		$('#section3').addClass('visible');
+	} else {
+		$('#section3').removeClass('visible');
+	}
+	
+	if ($(this).scrollTop() > (section1_height + section2_height + section3_height - 225) ) {
+		$('#section4').addClass('visible');
+	} else {
+		$('#section4').removeClass('visible');
+	}
+	
 });
 
 $(document).ready(function(){	
