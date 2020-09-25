2f3c6b89788c737a2f0a96740d6974e8baed83ed
Cris M <39185456+CristobalMontesino@users.noreply.github.com>
2020-09-25 15:05:47 +1000

    Add files via upload

--- works/m-site-5/js/swiper-javascript.js
+++ works/m-site-5/js/swiper-javascript.js
@@ -4,17 +4,17 @@
 /* global window */
 /* eslint no-mixed-spaces-and-tabs: ["error", "smart-tabs", "no-unused-vars"] */
 $(document).ready(function(){
-                var swiper = new Swiper('.swiper-container', {
-                    slidesPerView: 1,
-                    spaceBetween: 30,
-                    pagination: {
-                      clickable: true,
-                    },
-                    breakpoints: {
-                      980: {
-                        slidesPerView: 3,
-                        spaceBetween: 10
-                      }
-                    }
-                });
+	var swiper = new Swiper('.swiper-container', {
+    	slidesPerView: 1,
+    	spaceBetween: 30,
+    	pagination: {
+      	clickable: true,
+    	},
+    	breakpoints: {
+      	980: {
+        	slidesPerView: 3,
+        	spaceBetween: 10
+      	}
+    	}
+	});
 });
