a4ef512b588f74555c668e8b93fdf86ee539cd11
Cris M <39185456+CristobalMontesino@users.noreply.github.com>
2020-09-30 13:23:03 +1000

    Add files via upload

--- works/m-site-4/js/app-javascript.js
+++ works/m-site-4/js/app-javascript.js
@@ -1,1 +1,49 @@
-// JavaScript Document
+// variables
+// JavaScript v1.0
+/* global $ */
+/* global document */
+/* global window */
+/* eslint no-mixed-spaces-and-tabs: ["error", "smart-tabs", "no-unused-vars"] */
+
+const cartBtn = document.querySelector('.topnav-cart-btn');
+const closeCartBtn = document.querySelector('.shopcart-close-cart');
+const clearCartBtn = document.querySelector('.shopcart-clear-cart');
+const cartDOM = document.querySelector('.shopcart-cart');
+const cartOverlay = document.querySelector('.shopcart-overlay');
+const cartItems = document.querySelector('.topnav-cart-items');
+const cartTotal = document.querySelector('.shopcart-cart-total');
+const cartContent = document.querySelector('.cartitem-cart-content');
+const productsDOM = document.querySelector('.products-center');
+//
+let cart = [];
+
+// getting the products
+class Products{
+	async getProducts(){
+		try {
+			let result = await fetch('products.json');
+			return result;
+		} catch (error) {
+			console.log(error);
+		}
+	}
+}
+
+// display products
+class UI {
+	
+}
+
+// local storage
+class Storage {
+	
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	const ui = new UI();
+	const products = new Products();
+	
+	// get all products
+	products.getProducts().then(data => console.log(data));
+})
+
