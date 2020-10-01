e5f8f7fee213b237888b3b548b3f7b1e8b65e13a
Cris M <39185456+CristobalMontesino@users.noreply.github.com>
2020-10-01 11:39:06 +1000

    Add files via upload

--- works/m-site-4/js/app-javascript.js
+++ works/m-site-4/js/app-javascript.js
@@ -22,7 +22,8 @@ class Products{
 	async getProducts(){
 		try {
 			let result = await fetch('products.json');
-			return result;
+			let data = await result.json();
+			return data;
 		} catch (error) {
 			console.log(error);
 		}
