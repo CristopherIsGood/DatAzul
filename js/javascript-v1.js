7988d0cddeaa15a92cfcb3e9b2b9122d7b3e63f0
Cris M <39185456+CristobalMontesino@users.noreply.github.com>
2020-10-13 18:37:02 +1100

    Add files via upload

--- js/javascript-v1.js
+++ js/javascript-v1.js
@@ -3,15 +3,33 @@
 /* global document */
 /* global window */
 /* eslint no-mixed-spaces-and-tabs: ["error", "smart-tabs"] */
-window.onscroll = function() {myFunction()};
 
-function myFunction() {
-  if (document.body.scrollTop > 190 || document.documentElement.scrollTop > 190) {
-    document.getElementById("myDIV").className = "visible";
-  } else {
-    document.getElementById("myDIV").className = "hidden";
-  }
-}
+$(window).scroll(function(){
+	var section1 = $("#section1")  
+	var section2 = $("#section2")
+	var section3 = $("#section3")
+	var section1_height = section1.height();  
+	var section2_height = section2.height();
+	var section3_height = section3.height();
+	
+    if ($(this).scrollTop() > section1_height) {
+        $('#section2').addClass('visible');
+    } else {
+        $('#section2').removeClass('visible');
+	}
+	
+	if ($(this).scrollTop() > (section1_height + section2_height) ) {
+		$('#section3').addClass('visible');
+	} else {
+		$('#section3').removeClass('visible');
+	}
+	
+	if ($(this).scrollTop() > (section1_height + section2_height + section3_height) ) {
+		$('#section4').addClass('visible');
+	} else {
+		$('#section4').removeClass('visible');
+	}
+});
 
 $(document).ready(function(){
 	$('.slider-0').bxSlider({
