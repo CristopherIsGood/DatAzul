d222e70dd625208c0313a4df429bcdce8429bcdc
Cris M <39185456+CristobalMontesino@users.noreply.github.com>
2021-07-15 16:18:16 +1000

    Add files via upload

--- works/m-site-4/js/javascript-v1.js
+++ works/m-site-4/js/javascript-v1.js
@@ -1,1 +1,18 @@
+// JavaScript
+/* global $ */
+/* global document */
+/* global window */
+/* eslint no-mixed-spaces-and-tabs: ["error", "smart-tabs", "no-unused-vars"] */
 
+$(document).ready(function(){	
+	// remove class when it closes the drop down list after "getElementById("nav-drop_down")"
+	$('.navigation').removeClass('show');
+	
+    $("#nav-drop_down ul li a").click(function() {
+        $('.navigation').removeClass('show');
+    });
+});
+
+function myButton() {
+	document.getElementById("nav-drop_down").classList.toggle("show");
+}
---
+++ works/m-site-4/js/swiper-javascript.js
@@ -0,0 +1,20 @@
+// JavaScript v1.0
+/* global $ */
+/* global document */
+/* global window */
+/* eslint no-mixed-spaces-and-tabs: ["error", "smart-tabs", "no-unused-vars"] */
+$(document).ready(function(){
+	var swiper = new Swiper('.swiper-container', {
+    	slidesPerView: 1,
+    	spaceBetween: 30,
+    	pagination: {
+      	clickable: true,
+    	},
+    	breakpoints: {
+      	980: {
+        	slidesPerView: 3,
+        	spaceBetween: 10
+      	}
+    	}
+	});
+});
