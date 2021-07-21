6c3a1af75bd5c8e5ca3ea23b6470799899b03b01
Cris M <39185456+CristobalMontesino@users.noreply.github.com>
2021-07-21 12:37:06 +1000

    Add files via upload

--- works/m-site-4/js/javascript-v1.js
+++ works/m-site-4/js/javascript-v1.js
@@ -3,6 +3,10 @@
 /* global document */
 /* global window */
 /* eslint no-mixed-spaces-and-tabs: ["error", "smart-tabs", "no-unused-vars"] */
+/* exported openModal */
+/* exported closeModal */
+/* exported plusSlides */
+/* exported currentSlide */
 
 $(document).ready(function(){	
 	// remove class when it closes the drop down list after "getElementById("nav-drop_down")"
@@ -15,4 +19,44 @@ $(document).ready(function(){
 
 function myButton() {
 	document.getElementById("nav-drop_down").classList.toggle("show");
+}
+
+function openModal() {
+    document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById('myModal').style.display = "none";
+    $('video').trigger('pause');
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+    $('video').trigger('pause');
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+    $('video').trigger('pause');
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
 }
