071341046e36255f3ae7c836eefd138bb0e5d83c
Cris M <39185456+CristobalMontesino@users.noreply.github.com>
2021-07-21 14:13:36 +1000

    Add files via upload

--- works/m-site-1/js/javascript-v1.js
+++ works/m-site-1/js/javascript-v1.js
@@ -2,12 +2,15 @@
 /* global $ */
 /* global document */
 /* global window */
-/* eslint no-mixed-spaces-and-tabs: ["error", "smart-tabs"] */
 /* exported myAbout */
 /* exported myMenu1 */
 /* exported myMenu2 */
 /* exported myButton */
 /* eslint no-mixed-spaces-and-tabs: ["error", "smart-tabs", "no-unused-vars"] */
+/* exported openModal */
+/* exported closeModal */
+/* exported plusSlides */
+/* exported currentSlide */
 
 function myAbout() {
   $(window).scrollTop($('#about').offset().top);
@@ -54,4 +57,44 @@ $(document).ready(function(){
 
 function myButton() {
     document.getElementById("nav-drop_down").classList.toggle("show");
+}
+
+function openModal() {
+	document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+	document.getElementById('myModal').style.display = "none";
+	$('video').trigger('pause');
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+	$('video').trigger('pause');
+}
+
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+	$('video').trigger('pause');
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("demo");
+	var captionText = document.getElementById("caption");
+	if (n > slides.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " active";
+	captionText.innerHTML = dots[slideIndex-1].alt;
 }
